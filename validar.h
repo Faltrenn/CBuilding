06d3a1f5ba85f724edc62736afb4806d64b86a16
Emanuel Alves <emanuelalvesp5@gmail.com>
2023-10-02 23:27:01 -0300

    Adição da função de validar cpf.

--- validar.h
+++ validar.h
@@ -7,5 +7,6 @@ char* pegar_valor(char* mensagem, int tamanho, char* mensagem_erro);
 int pegar_inteiro(char* mensagem, int tamanho, char* mensagem_erro);
 bool validar_inteiro(char* inteiro);
 char* cortar_string(char *str, int inicio, int fim);
+int validar_cpf(char *cpf);
 
 #endif
